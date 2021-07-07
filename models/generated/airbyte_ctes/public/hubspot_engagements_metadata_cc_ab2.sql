f34cfa9af05146ac9f56336abe733319d31a56e8
Marcos Marx <marcosmarxm@gmail.com>
2021-07-07 09:07:02 -0300

    add poke project

--- models/generated/airbyte_ctes/public/hubspot_engagements_metadata_cc_ab2.sql
+++
@@ -1,9 +0,0 @@
-{{ config(schema="_airbyte_public", tags=["nested-intermediate"]) }}
--- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
-select
-    _airbyte_metadata_hashid,
-    cast(email as {{ dbt_utils.type_string() }}) as email,
-    _airbyte_emitted_at
-from {{ ref('hubspot_engagements_metadata_cc_ab1') }}
--- cc at hubspot_engagements/metadata/cc
-
--- models/generated/airbyte_ctes/public/hubspot_forms_formfieldgroups_richtext_ab2.sql
+++
@@ -1,9 +0,0 @@
-{{ config(schema="_airbyte_public", tags=["nested-intermediate"]) }}
--- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
-select
-    _airbyte_formfieldgroups_hashid,
-    cast({{ adapter.quote('content') }} as {{ dbt_utils.type_string() }}) as {{ adapter.quote('content') }},
-    _airbyte_emitted_at
-from {{ ref('hubspot_forms_formfieldgroups_richtext_ab1') }}
--- richtext at hubspot_forms/formFieldGroups/richText
-
